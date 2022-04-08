f72f747e46daeb0266a5de59476caa1035374c72
nfherrin <nfherrin@ncsu.edu>
2022-04-08 17:16:07 -0400

    Added output, now outputs hammer mesh files

--- src/thrm2hammer.f95
+++ src/thrm2hammer.f95
@@ -4,7 +4,7 @@ PROGRAM thrm2hammer
   INTEGER(8) :: num_points,num_tets
   CHARACTER(64) :: mesh_file
   INTEGER(8),ALLOCATABLE :: tetgeom(:,:)
-  REAL(8),ALLOCATABLE :: point(:,:),tetvol(:)
+  REAL(8),ALLOCATABLE :: point(:,:)
   REAL(8),PARAMETER :: PI=4.D0*DATAN(1.D0)
   INTEGER(8),PARAMETER::in_unit=20,out_unit=30
 
@@ -13,6 +13,8 @@ PROGRAM thrm2hammer
   CALL readin_mesh()
 
   CALL computevol()
+
+  CALL outputthrm()
 
 CONTAINS
 
@@ -34,8 +36,8 @@ CONTAINS
   SUBROUTINE readin_mesh()
     INTEGER(8) :: i,tempint
     !read in the mesh file
-    OPEN(UNIT = in_unit, FILE = mesh_file, ACTION = "read", IOSTAT=tempint)
-    IF(tempint .NE. 0)STOP 'error opening mesh file'
+    OPEN(UNIT = in_unit, FILE = mesh_file, ACTION = "read", STATUS='OLD', IOSTAT=tempint)
+    IF(tempint .NE. 0)STOP 'error opening input mesh file'
 
     READ(in_unit,*)num_points
     READ(in_unit,*)num_tets
@@ -65,11 +67,9 @@ CONTAINS
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   SUBROUTINE computevol()
     INTEGER(8) :: i
-    REAL(8) :: totalvol1
+    REAL(8) :: totalvol1,tetvol
     REAL(8) :: a(3),b(3),c(3),d(3)
 
-    ALLOCATE(tetvol(num_tets))
-    tetvol(:)=0
     totalvol1=0
     !compute the tet volumes
     DO i=1,num_tets
@@ -77,13 +77,53 @@ CONTAINS
       b(:)=point(tetgeom(i,2),:)
       c(:)=point(tetgeom(i,3),:)
       d(:)=point(tetgeom(i,4),:)
-      tetvol(i)=ABS((-c(2)*d(1)+b(2)*(-c(1)+d(1))+b(1)*(c(2)-d(2))+c(1)*d(2))*(a(3)-d(3))+(a(1)-d(1)) &
+      tetvol=ABS((-c(2)*d(1)+b(2)*(-c(1)+d(1))+b(1)*(c(2)-d(2))+c(1)*d(2))*(a(3)-d(3))+(a(1)-d(1)) &
         *(-c(3)*d(2)+b(3)*(-c(2)+d(2))+b(2)*(c(3)-d(3))+c(2)*d(3))+(a(2)-d(2))*(b(3)*(c(1)-d(1)) &
         +c(3)*d(1)-c(1)*d(3)+b(1)*(-c(3)+d(3))))/6
-      totalvol1=totalvol1+tetvol(i)
+      totalvol1=totalvol1+tetvol
     ENDDO
     WRITE(*,'(A,ES24.16)')'total volume:      ',totalvol1
     WRITE(*,'(A,ES24.16)')'equivalent radius: ',(3.0/4.0/pi*totalvol1)**(1.0/3.0)
   ENDSUBROUTINE computevol
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  SUBROUTINE outputthrm()
+    CHARACTER(64) :: outfile
+    INTEGER(8) :: i,tempint
+    !open the output mesh file
+    WRITE(outfile,'(2A)')TRIM(ADJUSTL(mesh_file)),'.xml'
+    OPEN(UNIT = out_unit, FILE = outfile, ACTION = "WRITE", STATUS="REPLACE", IOSTAT=tempint)
+    IF(tempint .NE. 0)STOP 'error opening output mesh file'
+
+    !initial wrapping stuff
+    WRITE(out_unit,'(A)')"<?xml version='1.0' encoding='UTF-8'?>"
+    WRITE(out_unit,*)
+    WRITE(out_unit,'(A)')"<hammer>"
+    WRITE(out_unit,'(A)')"  <external_meshes>"
+    WRITE(out_unit,'(A,I0,A,I0,A)')'    <unstructured_tetrahedral_mesh name="my_tet_mesh" bins="', &
+      num_tets,'" k="',num_tets,'" adjacency_info="false">'
+    WRITE(out_unit,'(A)')"      <vertices>"
+    !write out the points
+    DO i=1,num_points
+      WRITE(out_unit,'(A,3ES24.16)')'       ',point(i,:)
+    ENDDO
+    WRITE(out_unit,'(A)')"      </vertices>"
+    WRITE(out_unit,'(A)')"      <elements>"
+    !write out each tet
+    DO i=1,num_tets
+      WRITE(out_unit,'(A,I0,A,I0,A,I0,A,I0,A,I0,A)')'        <e id="',i-1,'">',tetgeom(i,1)-1,' ', &
+        tetgeom(i,2)-1,' ',tetgeom(i,3)-1,' ',tetgeom(i,4)-1,'</e>'
+    ENDDO
+    !closing wrapups
+    WRITE(out_unit,'(A)')"      </elements>"
+    WRITE(out_unit,'(A)')'    </unstructured_tetrahedral_mesh>'
+    WRITE(out_unit,'(A)')"  </external_meshes>"
+    WRITE(out_unit,'(A)')"</hammer>"
+
+    WRITE(*,'(A)')'Program completed without error'
+    WRITE(*,'(3A)')'Output mesh written to "',TRIM(ADJUSTL(outfile)),'"'
+
+    CLOSE(out_unit)
+  ENDSUBROUTINE outputthrm
 
 END PROGRAM thrm2hammer
